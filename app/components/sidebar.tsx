a83a7d668a870bcaa6f47412f56b549803a3b604
Tomda <67380286+Tomccc520@users.noreply.github.com>
2023-06-13 12:03:29 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Tomda</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          一个爱代码的设计师.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
